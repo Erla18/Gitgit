75605d48c27bf7a7bbe31923fa496deede234614
Erik <eriiikl@hotmail.com>
2021-12-07 13:08:22 +0100

    Initial commit Added Change1 Change 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-	System.out.println("Hello World Change hahahahahahah 3");
+	System.out.println("Hello World Change hahahanejnej 3");
     }
 
 }
